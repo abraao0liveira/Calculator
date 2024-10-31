b742b26314f697f89fab6d19f7d5422f97923400
Abraão Oliveira <abraaooliveira.br@gmail.com>
2024-10-31 00:57:08 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear(); // Clear the console
+      Console.WriteLine("Welcome to the calculator!");
+
+      Console.WriteLine("Enter the first number:");
+      float num01 = Convert.ToSingle(Console.ReadLine());
+
+      Console.WriteLine("Enter the second number:");
+      float num02 = Convert.ToSingle(Console.ReadLine());
+
+      float result = num01 + num02;
+
+      Console.WriteLine($"The result is: {result}!"); // Interpolation
+    }
+  }
+}
